8750b1e83d3d23ef490c3724a816eb3ade971cc7
LNTR <101091694+LNTR@users.noreply.github.com>
2024-02-26 13:34:48 +0530

    Linear model tested

--- test.py
+++ test.py
@@ -9,6 +9,17 @@ def weighted_moving_average(a, window=3):
     return np.convolve(a, weights, mode="valid")
 
 
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+def weighted_moving_average(a, window=3):
+    weights = [(1 / window) for _ in range(1, window + 1)]
+    return np.convolve(a, weights, mode="valid")
+
+
 class LinearRegression:
     def __init__(self, x_vector, y_vector):
         self.x_vector = np.array(x_vector)
@@ -34,12 +45,11 @@ class LinearRegression:
 
         return gradient_matrix
 
-    def fit(self, step_size=0.0001, num_iterations=10000):
+    def fit(self, step_size=0.001, num_iterations=10000):
         for i in range(1, num_iterations):
-            step_size /= math.sqrt(i)
             gradient_matrix = self._get_gradient_matrix()
-            self.w0 += step_size * gradient_matrix[0]
-            self.w1 += step_size * gradient_matrix[1]
+            self.w0 -= step_size * gradient_matrix[0]
+            self.w1 -= step_size * gradient_matrix[1]
 
     def _show_coeffiecients(self):
         print(f"w0: {self.w0}\nw1: {self.w1}\n")
@@ -54,12 +64,12 @@ x = [
     2,
     3,
 ]
-y = [1, 2, 3]
+y = [6, 9, 12]
 model = LinearRegression(x, y)
-model.fit(num_iterations=10000)
+model.fit(num_iterations=100000)
 
+model._show_coeffiecients()
+# plt.plot(x, y)
+# plt.plot(x, model.predict(np.array(x)))
 
-plt.plot(x, y)
-plt.plot(x, model.predict(np.array(x)))
-
-plt.show()
+# plt.show()
