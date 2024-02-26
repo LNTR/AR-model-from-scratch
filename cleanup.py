8474f1897af5f71ce0e820a72e8219421e11fe31
LNTR <101091694+LNTR@users.noreply.github.com>
2024-02-26 17:05:00 +0530

    modified:   cleaned_inputs.xlsx 	modified:   cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,12 @@
 # used for cleaning and transforming the inputs inside the excel
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def weighted_moving_average(a, window=3):
+    weights = [(1 / window) for _ in range(1, window + 1)]
+    return np.convolve(a, weights, mode="valid")
 
 
 df = pd.read_excel("inputs.xlsx")
@@ -10,8 +16,9 @@ df["Date (mm-dd-yyyy)"] = pd.to_datetime(df["Date (mm-dd-yyyy)"]).dt.strftime(
 )
 cleand_df = df.groupby("Date (mm-dd-yyyy)").sum()
 cleand_df.to_excel("cleaned_inputs.xlsx")
-cleand_df["400g Crystal"]
-plt.plot(cleand_df.index, cleand_df["400g Crystal"].to_list())
+
+plt.plot(cleand_df.index[1:-1], cleand_df["400g Crystal"].to_list()[1:-1])
+without_noise = weighted_moving_average(cleand_df["400g Crystal"])
+plt.plot(cleand_df.index[1:-1], without_noise)
 
 plt.show()
-# print()
