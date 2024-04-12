e517a314567163b1414b70c6e2ebcb221270bad5
LNTR <101091694+LNTR@users.noreply.github.com>
2024-04-12 14:39:57 +0530

    Finished ACF calculation

--- acf.py
+++ acf.py
@@ -1,27 +1,71 @@
-def calculate_correlation(var1, var2):
+import numpy as np
+from matplotlib import pyplot as plt
 
-    if len(var1) != len(var2):
-        raise ValueError("Both variables should have the same number of data points.")
-    correlation_matrix = np.corrcoef(var1, var2)
-    correlation = correlation_matrix[0, 1]
-    return correlation
+data = [
+    451,
+    326,
+    319,
+    398,
+    427,
+    396,
+    309,
+    483,
+    162,
+    368,
+    200,
+    190,
+    737,
+    699,
+    124,
+    683,
+    284,
+    554,
+    223,
+    471,
+    221,
+    566,
+    577,
+    546,
+    327,
+    589,
+]
 
 
-collerations = np.array([])
-lag_size = 6
-data_length = len(data)
+class AutoCorrelationCalculator:
 
-for i in range(1, 5):
-    collerations = np.append(
-        collerations,
-        abs(
-            calculate_correlation(
-                data[data_length - lag_size - i : data_length - i],
-                data[data_length - lag_size - i - 1 : data_length - i - 1],
+    def __init__(self, data, lag_size=10) -> None:
+        self.data = data
+        self.lag_size = lag_size
+        self.collerations = np.array([])
+
+    def _calculate_correlation(self, var1, var2):
+
+        if len(var1) != len(var2):
+            raise ValueError(
+                "Both variables should have the same number of data points."
             )
-        ),
-    )
+        correlation_matrix = np.corrcoef(var1, var2)
+
+        correlation = correlation_matrix[0, 1]
+
+        return correlation
+
+    def calculate_acf(self, t):
+        data_length = len(self.data)
+
+        for i in range(1, t + 1):
+            self.collerations = np.append(
+                self.collerations,
+                self._calculate_correlation(
+                    self.data[data_length - self.lag_size - i : data_length - i],
+                    self.data[data_length - self.lag_size : data_length],
+                ),
+            )
+
+    def get_acf_vector(self):
+        return self.collerations
 
 
-collerations = np.sort(collerations)[::-1]
-print(collerations)
+acf = AutoCorrelationCalculator(data, 10)
+acf.calculate_acf(7)
+print(acf.get_acf_vector())
--- arima.py
+++ arima.py
@@ -1,37 +1,87 @@
 import numpy as np
 import pandas as pd
-import math
 import matplotlib.pyplot as plt
 
 
-class PolynomialRegression:
+class ARMA:
     def __init__(self, y_vector):
-        self.y_vector = np.array(y_vector, dtype=np.float64)
-        self.error_list = np.array([0, 0])
         self.start_index = 2
+        self.y_vector = np.array(y_vector, dtype=np.float64)
+        self.error_vector = np.array([0, 0], dtype=np.float64)
 
-        self.w0 = 0
-        self.w1 = 0
-        self.w2 = 0
-        self.w3 = 0
-        self.w4 = 0
+        self.index_vector = np.arange(self.start_index, len(self.y_vector))
+
+        self.c = 0
+        self.r1 = 0
+        self.r2 = 0
+        self.e1 = 0.5
+        self.e2 = 0.5
 
         self.epsilon = 1
 
         self.m = 0.25
         self.v = 0.25
 
-        self.momentum = np.zeros(5)
-        self.velocity = np.zeros(5)
+        self.momentum = np.zeros(3)
+        self.velocity = np.zeros(3)
 
-    def _get_predicted_value_matrix(self, index):
-        ar = self.w1 * self.y_vector[index - 1] + self.w2 * self.y_vector[index - 2]
-        ma = self.w3 * self.error_list[index - 1] + self.w4 * self.error_list[index - 2]
-        y = self.w0 + ar + ma
+    def _get_predicted_value(self, index):
+        y = self.r1 * self.y_vector[index - 1]
         return y
 
+    def _get_gradient_matrix(self):
+        predicted_value_vector = []
+        for index in self.index_vector:
+            predicted_value_vector.append(self._get_predicted_value(index))
+
+        gradient_matrix = [
+            sum((self.y_vector[self.start_index :] - predicted_value_vector)),
+            sum(
+                (self.y_vector[self.start_index :] - predicted_value_vector)
+                * self.y_vector[self.start_index - 1 : len(self.y_vector) - 1]
+            ),
+            sum(
+                (self.y_vector[self.start_index :] - predicted_value_vector)
+                * (self.y_vector[self.start_index - 2 : len(self.y_vector) - 2])
+            ),
+        ]
+
+        gradient_matrix = np.array(gradient_matrix, dtype=np.float64) * -2
+
+        return gradient_matrix
+
+    def fit(self, step_size=0.001, num_iterations=100000):
+        self.step_size = step_size
+        self.error_vector = np.array([0, 0], dtype=np.float64)
+
+        for _ in range(1, num_iterations):
+            self._update_momentum()
+            self._update_velocity()
+            self.c -= (
+                self.step_size
+                * self.momentum[0]
+                / (self.epsilon + np.sqrt(self.velocity[0]))
+            )
+            self.r1 -= (
+                self.step_size
+                * self.momentum[1]
+                / (self.epsilon + np.sqrt(self.velocity[1]))
+            )
+            self.r2 -= (
+                self.step_size
+                * self.momentum[2]
+                / (self.epsilon + np.sqrt(self.velocity[2]))
+            )
+
+    def _show_coeffiecients(self):
+        print(f"c: {self.c}\tr1: {self.r1}\tr2: {self.r2}\t\t")
+
+    def view_inside(self):
+        y_vals = self._get_predicted_value(self.index_vector)
+        return y_vals
+
     def _get_current_weigts(self):
-        return np.array([self.w0, self.w1, self.w2, self.w3])
+        return np.array([self.c, self.r1, self.r2])
 
     def _update_momentum(self):
         gradient_matrix = self._get_gradient_matrix()
@@ -40,70 +90,6 @@ class PolynomialRegression:
     def _update_velocity(self):
         gradient_matrix = self._get_gradient_matrix()
         self.velocity = self.v * self.velocity + (1 - self.v) * (gradient_matrix**2)
-
-    def _get_gradient_matrix(self):
-        predicted_value_vector = self._get_predicted_value_matrix(self.x_vector)
-        gradient_matrix = [
-            sum((self.y_vector - predicted_value_vector)),
-            sum(
-                (self.y_vector - predicted_value_vector)
-                * self.y_vector[self.start_index - 1 :]
-            ),
-            sum(
-                (self.y_vector - predicted_value_vector)
-                * (self.y_vector[self.start_index - 2 :])
-            ),
-            sum(
-                (self.y_vector - predicted_value_vector)
-                * (self.error_list[self.start_index - 1 :])
-            ),
-            sum(
-                (self.y_vector - predicted_value_vector)
-                * (self.error_list[self.start_index - 2 :])
-            ),
-        ]
-
-        gradient_matrix = np.array(gradient_matrix, dtype=np.float64) * -2
-
-        return gradient_matrix
-
-    def fit(self, step_size=1, num_iterations=100000):
-        self.step_size = step_size
-        for _ in range(1, num_iterations):
-            self._update_momentum()
-            self._update_velocity()
-            self.w0 -= (
-                self.step_size
-                * self.momentum[0]
-                / (self.epsilon + np.sqrt(self.velocity[0]))
-            )
-            self.w1 -= (
-                self.step_size
-                * self.momentum[1]
-                / (self.epsilon + np.sqrt(self.velocity[1]))
-            )
-            self.w2 -= (
-                self.step_size
-                * self.momentum[2]
-                / (self.epsilon + np.sqrt(self.velocity[2]))
-            )
-            self.w3 -= (
-                self.step_size
-                * self.momentum[3]
-                / (self.epsilon + np.sqrt(self.velocity[3]))
-            )
-            self.w3 -= (
-                self.step_size
-                * self.momentum[4]
-                / (self.epsilon + np.sqrt(self.velocity[4]))
-            )
-
-    def _show_coeffiecients(self):
-        print(f"w0: {self.w0}\tw1: {self.w1}\tw2: {self.w2}\tw3: {self.w3}\t")
-
-    def predict(self, x):
-        y = self.w0 + self.w1 * x + self.w2 * (x**2) + self.w3 * (x**3)
-        return y
 
 
 y = [
@@ -122,21 +108,16 @@ y = [
     699,
     124,
 ]
-x = list(range(1, len(y) + 1))
 
-model = PolynomialRegression(x, y)
+model = ARMA(np.log10(y))
 model.fit(num_iterations=100000)
+inside = model.view_inside()
+inside = np.power(10, inside[3:])
+y = np.array(y[5:])
 
-plt.plot(x, y, label="Actual")
-plt.plot(x, model.predict(np.array(x)), label="Predict")
-# plt.legend()
-# model._show_coeffiecients()
+print(y)
+print(inside[3:])
+plt.plot(y, label="Actual")
+plt.plot(inside, label="Predict")
+plt.legend()
 plt.show()
-# np.set_printoptions(suppress=True, precision=3)
-
-# pred = model.predict(np.array([1000, -1500, -9635, 9635], dtype=np.float64))
-# test = np.array([f(x_val) for x_val in [1000, -1500, -9635, 9635]], dtype=np.float64)
-# accuracy = 1 - (sum((test - pred) ** 2) / sum((test - test.mean()) ** 2))
-# print(np.array_str(pred, precision=3, suppress_small=True))
-# print(np.array_str(test, precision=3, suppress_small=True))
-# print(accuracy)
